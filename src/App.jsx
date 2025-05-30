8b931c944670feef04fc0746f6e551af733bc9ba
arielmusa <arielmusainfo@gmail.com>
2025-05-30 16:43:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./components/Main";
+import "./assets/css/App.css";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,33 @@
+const apiUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+
+export default function Main() {
+  return (
+    <div className="container my-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+          ></input>
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          ></input>
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
